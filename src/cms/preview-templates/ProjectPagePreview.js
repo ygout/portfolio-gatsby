938f40e6a3967a79ab93453301282def5f745ad9
ygout <yohann.goutaret@gmail.com>
2019-02-02 17:56:24 +0100

    gallery preview template

--- src/cms/preview-templates/ProjectPagePreview.js
+++ src/cms/preview-templates/ProjectPagePreview.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { ProjectPageTemplate } from '../../templates/project-page'
 
-const ProjectPagePreview = ({ entry, widgetFor }) => (
-  <ProjectPageTemplate
-    title={entry.getIn(['data','title'])}
-    description={entry.getIn(['data', 'description'])}
-    tools={entry.getIn(['data', 'outils'])}
-    langages={entry.getIn(['data', 'langages'])}
-    results={entry.getIn(['data', 'results'])}
-    gallery={entry.getIn(['data', 'gallery'])}
-    thumbnail={entry.getIn(['data', 'thumbnail'])}
-  />
-)
+const ProjectPagePreview = ({ entry, widgetFor }) => {
+
+  const entryGallery = entry.getIn(['data', entry]);
+  const gallery = entryGallery ? entryGallery.toJs(): []
+  return (
+    <ProjectPageTemplate
+      title={entry.getIn(['data','title'])}
+      description={entry.getIn(['data', 'description'])}
+      tools={entry.getIn(['data', 'outils'])}
+      langages={entry.getIn(['data', 'langages'])}
+      results={entry.getIn(['data', 'results'])}
+      gallery={entry.getIn(['data', 'gallery'])}
+      thumbnail={entry.getIn(['data', 'thumbnail'])}
+    />
+  )
+}
+  
 
 export default ProjectPagePreview
