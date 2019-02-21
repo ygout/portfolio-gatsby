5a1ec8f4e606957f4ba3349c68a201a055cb7c75
ygout <yohann.goutaret@gmail.com>
2019-02-21 14:24:17 +0100

    add mr-4 child div

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -7,16 +7,18 @@ export default class ProjectCard extends React.Component {
     const { project } = this.props;
     return (
       <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 cell-container">
-        <img alt={project.frontmatter.title} className="image" src={project.frontmatter.thumbnail} />
-        <Link
-          className="overlay"
-          to={project.fields.slug}
-          target="_blank" rel="noopener noreferrer"
-        >
-          <div className="text">
-            <p className="sub-title">{project.frontmatter.title}</p>
-          </div>
-        </Link>
+        <div className="mr-4">
+          <img alt={project.frontmatter.title} className="image mr-4" src={project.frontmatter.thumbnail} />
+          <Link
+            className="overlay"
+            to={project.fields.slug}
+            target="_blank" rel="noopener noreferrer"
+          >
+            <div className="text">
+              <p className="sub-title">{project.frontmatter.title}</p>
+            </div>
+          </Link>
+        </div>
       </div>
     )
   }
