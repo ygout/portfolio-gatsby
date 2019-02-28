dbf1f590c53efaa753432d06bf7eb831237d9e3c
ygout <yohann.goutaret@gmail.com>
2019-02-28 16:22:24 +0100

    overlay for projectCard adapting now at image and not div

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -8,16 +8,19 @@ export default class ProjectCard extends React.Component {
     return (
       <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 cell-container">
         <div className="mr-4">
-          <img alt={project.frontmatter.title} className="image mr-4" src={project.frontmatter.thumbnail} />
-          <Link
-            className="overlay"
-            to={project.fields.slug}
-            target="_blank" rel="noopener noreferrer"
-          >
-            <div className="text">
-              <p className="sub-title">{project.frontmatter.title}</p>
-            </div>
-          </Link>
+          <div className="col-md-12">
+            <img alt={project.frontmatter.title} className="image" src={project.frontmatter.thumbnail} >
+            </img>
+            <Link
+              className="overlay"
+              to={project.fields.slug}
+              target="_blank" rel="noopener noreferrer"
+            >
+              <div className="text">
+                <p className="sub-title">{project.frontmatter.title}</p>
+              </div>
+            </Link></div>
+
         </div>
       </div>
     )
