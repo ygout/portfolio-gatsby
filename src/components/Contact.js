151f95093774d848d66cadd9ba53e1ac17335f6f
ygout <yohann.goutaret@gmail.com>
2019-03-01 10:19:44 +0100

    Recaptcha -test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ import { navigateTo } from "gatsby-link";
 import Recaptcha from "react-google-recaptcha";
 
 const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
-
+console.log('RECAPTCHA_KEY', RECAPTCHA_KEY);
 function encode(data) {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
